abb7ef27e7b75da16a61242ff437db683ef83665
Bivar <bivardourado@gmail.com>
2023-03-06 11:53:42 -0300

    atualizaçoes

---
+++ src/main/java/br/com/training/controller/dto/UserForm.java
@@ -0,0 +1,60 @@
+package br.com.training.controller.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public class UserForm {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String cpf;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotNull
+    @Past
+    @JsonFormat(pattern="dd/MM/yyyy")
+    private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
--- src/main/java/br/com/training/service/UserService.java
+++ src/main/java/br/com/training/service/UserService.java
@@ -2,9 +2,12 @@ package br.com.training.service;
 
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.training.controller.dto.UserForm;
+import br.com.training.controller.dto.UserResponse;
 import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
 
@@ -14,33 +17,46 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-    public User findUserByCpf(String cpf) {
-        return userRepository.findByCpf(cpf);
-    }
-    
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public User updateUserByCpf(String cpf,  User user) {
+    public UserResponse saveUser(UserForm userForm) {
+        User user = convertToEntity(userForm);
+        User savedUser = userRepository.save(user);
+        return convertToDto(savedUser);
+    }
+
+    private User convertToEntity(UserForm userForm) {
+        return modelMapper.map(userForm, User.class);
+    }
+
+    private UserResponse convertToDto(User user) {
+        return modelMapper.map(user, UserResponse.class);
+    }
+
+    public UserResponse findUserByCpf(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        return convertToDto(user);
+    }
+
+    public UserResponse updateUserByCpf(String cpf, UserForm userForm) {
         User existingUser = userRepository.findByCpf(cpf);
         if (existingUser == null) {
-        
             return null;
         }
+        User user = convertToEntity(userForm);
         existingUser.setName(user.getName());
         existingUser.setEmail(user.getEmail());
         existingUser.setBirthDate(user.getBirthDate());
-       
-        return userRepository.save(existingUser);
+        User updatedUser = userRepository.save(existingUser);
+        return convertToDto(updatedUser);
     }
 
     public void deleteUserByCpf(String cpf) {
         User existingUser = userRepository.findByCpf(cpf);
         if (existingUser == null) {
-      
-        	userRepository.delete(existingUser);
+            userRepository.delete(existingUser);
         }
-        
+    
     }
 }
