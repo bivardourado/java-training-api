9ecad4ae86f7f76427582416309d8dfc40d5bc5b
Bivar <bivardourado@gmail.com>
2023-03-13 17:44:58 -0300

    Implementação testes unitários

--- src/main/java/br/com/training/controller/UserController.java
+++ src/main/java/br/com/training/controller/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import br.com.training.controller.dto.UserForm;
-import br.com.training.controller.dto.UserResponse;
+import br.com.training.model.User;
+
 import br.com.training.service.UserService;
 
 @RestController
@@ -20,25 +20,26 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserResponse createUser(@RequestBody @Valid UserForm userForm) {
-        return userService.createUser(userForm);
+    public User createUser(@RequestBody @Valid User user) {
+        return userService.saveUser(user);
     }
 
-    @GetMapping(value = "/{cpf}")
+    @GetMapping (value = "/{cpf}")
     @ResponseStatus(HttpStatus.OK)
-    public UserResponse getUser(@PathVariable Long cpf) {
-        return userService.getUserById(cpf);
+    public User getUser (@PathVariable String cpf){
+        return userService.findUserByCpf(cpf);
+        
     }
 
-    @PutMapping(value = "/{cpf}")
+    @PutMapping (value = "/{cpf}")
     @ResponseStatus(HttpStatus.OK)
-    public UserResponse updateUser(@PathVariable Long cpf, @RequestBody @Valid UserForm userForm) {
-        return userService.updateUser(cpf, userForm);
+    public User updateUser(@PathVariable String cpf, @RequestBody @Valid User user) {
+        return userService.updateUserByCpf(cpf, user);
     }
 
-    @DeleteMapping(value = "/{cpf}")
+    @DeleteMapping (value = "/{cpf}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long cpf) {
-        userService.deleteUser(cpf);
+    public void deleteUser(@PathVariable String cpf) {
+        userService.deleteUserByCpf(cpf);
     }
 }
--- src/main/java/br/com/training/service/UserService.java
+++ src/main/java/br/com/training/service/UserService.java
@@ -1,10 +1,10 @@
 package br.com.training.service;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.training.controller.dto.UserForm;
-import br.com.training.controller.dto.UserResponse;
 import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
 
@@ -14,62 +14,33 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public UserResponse createUser(UserForm userForm) {
-        User user = toUser(userForm);
-        User savedUser = userRepository.save(user);
-        UserResponse userResponse = toUserResponse(savedUser);
-        return userResponse;
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
-
-    public UserResponse getUserById(Long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return null;
-        }
-        return toUserResponse(user);
+    public User findUserByCpf(String cpf) {
+        return userRepository.findByCpf(cpf);
     }
+    
 
-    public UserResponse updateUser(Long id, UserForm userForm) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            return null;
-        }
-        updateUserFromDTO(user, userForm);
-        User updatedUser = userRepository.save(user);
-        UserResponse userResponse = toUserResponse(updatedUser);
-        return userResponse;
-    }
-
-    public void deleteUser(Long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user != null) {
-            userRepository.deleteById(id);
-        }
-    }
-
-
-    private User toUser(UserForm userForm) {
-        User user = new User();
-        user.setName(userForm.getName());
-        user.setEmail(userForm.getEmail());
-        user.setCpf(userForm.getCpf());
-        user.setBirthDate(userForm.getBirthDate());
-        return user;
-    }
-
-    private void updateUserFromDTO(User user, UserForm userForm) {
-        user.setName(userForm.getName());
-        user.setEmail(userForm.getEmail());
-    }
-
-    private UserResponse toUserResponse(User user) {
-        UserResponse userResponse = new UserResponse();
+    public User updateUserByCpf(String cpf,  User user) {
+        User existingUser = userRepository.findByCpf(cpf);
+        if (existingUser == null) {
         
-        userResponse.setName(user.getName());
-        userResponse.setEmail(user.getEmail());
-        userResponse.setCpf(user.getCpf());
-        userResponse.setBirthDate(user.getBirthDate());
-        return userResponse;
+            return null;
+        }
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setBirthDate(user.getBirthDate());
+       
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUserByCpf(String cpf) {
+        User existingUser = userRepository.findByCpf(cpf);
+        if (existingUser == null) {
+      
+        	userRepository.delete(existingUser);
+        }
+        
     }
 }
-
